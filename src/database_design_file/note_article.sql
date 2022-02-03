796c65e7b0fa9fdd0cec57077cdcf7af2df5b558
Cauchy <mailcauchy@gmail.com>
2022-02-03 22:17:05 +0800

    表结构修改

--- src/database_design_file/note_article.sql
+++ src/database_design_file/note_article.sql
@@ -8,8 +8,9 @@ CREATE TABLE `note_article`  (
   `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
   `author_id` bigint(20) NOT NULL,
   `create_time` timestamp(0) NULL DEFAULT NULL,
-  `status` tinyint(255) UNSIGNED NOT NULL,
+  `modify_time` timestamp(0) NULL DEFAULT NULL,
+  `status` tinyint(255) UNSIGNED NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
