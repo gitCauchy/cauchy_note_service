68ca0caca2d00339e8d2dca500ec649ad4770f39
Cauchy <mailcauchy@gmail.com>
2022-02-04 15:10:57 +0800

    数据库表结构修改

--- src/database_design_file/note_menu.sql
+++ src/database_design_file/note_menu.sql
@@ -8,9 +8,8 @@ DROP TABLE IF EXISTS `note_menu`;
 CREATE TABLE `note_menu`  (
   `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `path` char(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `name` char(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `label` char(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `icon` char(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `icon` char(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
-
-SET FOREIGN_KEY_CHECKS = 1;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
